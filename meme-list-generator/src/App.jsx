e1883b1535abba837c27e7e001e9936a2743ac03
PaulManRobertson <bigwallpaul514@gmail.com>
2022-12-08 15:36:23 -0700

    Meme component

--- meme-list-generator/src/App.jsx
+++ meme-list-generator/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Meme from "./components/Meme"
 
 import './App.css'
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Meme />
     </div>
   )
 }
