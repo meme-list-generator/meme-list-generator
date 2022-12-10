d62ab08b3c744d9c31932c242e4a902c1c6debb8
Jordan Burger <jordanburger22@gmail.com>
2022-12-10 14:56:58 -0700

    delete button working

--- meme-list-generator/src/components/Meme.jsx
+++ meme-list-generator/src/components/Meme.jsx
@@ -2,36 +2,53 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import MemeList from "./MemeList"
 
+
+
 export default function Meme() {
+
+    const [count, setCount] = useState(0)
+
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
-        url: "https://i.imgflip.com/345v97.jpg"
+        url: "",
+        id: "",
+        
     })
 
-    const [memeImages, setMemeImages] = useState([])
-    const [count, setCount] = useState(0)
+    // const [memeImages, setMemeImages] = useState([])
     const [arrOfMemes, setArrOfMemes] = useState([])
     const [memeList, setMemeList] = useState([])
 
-    
+
 
     React.useEffect(() => {
         axios.get("https://api.imgflip.com/get_memes")
-            .then(res => setArrOfMemes(res.data.data.memes))
+            .then(res => {
+                setArrOfMemes(res.data.data.memes)
+                setMeme(prevMeme => ({
+                    ...prevMeme,
+                    url: res.data.data.memes[0].url,
+                    id: res.data.data.memes[0].id
+                }))
+            })
+
     }, [])
-    
+
     function getMemeImages() {
         const randomNumber = Math.floor(Math.random() * arrOfMemes.length)
         const imgUrl = arrOfMemes[randomNumber].url
+        const imgId = arrOfMemes[randomNumber].id
         setMeme(prevMeme => ({
             ...prevMeme,
-            url: imgUrl
+            url: imgUrl,
+            id: imgId
+            
         }))
-       
+
     }
-    
-    console.log(meme)
+
+
 
     // useEffect(() => {
     //     getMemeImages()
@@ -45,34 +62,52 @@ export default function Meme() {
         }))
     }
 
-    // function countMemes() {
-    //     setCount(prevCount => prevCount + 1)
-        
-    // }
 
-   
 
     // function countMemes() {
     //     setCount(prevCount => prevCount + 1)
-        
+
     // }
 
-   
+
     function addMemeToList() {
-        return(
-            setMemeList(prevState => [...prevState, meme])
-        )
-        
+
+        setMemeList(prevState => {
+
+            if (prevState.find(item => item.id === meme.id)) {
+                return [...prevState]
+            } else {
+                return [...prevState, meme]
+            }
+
+        })
+
+
+    }
+
+
+
+    const deleteBtn = id => {
+        setMemeList(prevMemeList => {
+            return prevMemeList.filter(meme => meme.id !== id)
+        })
     }
     
+    function editHandler(){
+        setMemeList(prevMemeList => {
+            prevMemeList.find(item => item.id === meme.id)
+        })
+    }
 
-    const savedMemes = memeList.map(info =>(
+    const savedMemes = memeList.map(info => (
         <MemeList
-            info = {info}
-            key = {info.url}
-
+            info={info}
+            key={info.id}
+            delete = {deleteBtn}
+            
         />
     ))
+
 
 
     // function testing() {
@@ -80,7 +115,10 @@ export default function Meme() {
     // }
 
     // const memeListElement = arrOfMemes.map(data => <MemeList data={data}/>)
-    
+
+
+
+
     return (
         <main>
             <div className="form">
@@ -108,17 +146,18 @@ export default function Meme() {
                 </button>
                 {/* {<button onClick={testing}>TEST</button>} */}
                 {/* {<button onClick={testing}>TEST</button>} */}
-                <button 
-                    className="form-button" 
+                <button
+                    className="form-button"
                     onClick={addMemeToList}
+
                 >
                     Add this meme to your collection
                 </button>
             </div>
             <div className="meme">
-                <img 
-                    src={meme.url} 
-                    className="meme-image" 
+                <img
+                    src={meme.url}
+                    className="meme-image"
                 />
                 <h2 className="meme-text top">{meme.topText}</h2>
                 <h2 className="meme-text bottom">{meme.bottomText}</h2>
